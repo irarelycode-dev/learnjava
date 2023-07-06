7f6bf82a05f2e46d414c89406c8bbe61a16a9783
irarelycode-dev <irarelycode@gmail.com>
2023-07-05 20:44:47 -0400

    matrix ctci

--- src/com/datastructures/crackingthecodinginterview/arraysandstrings/ZeroMatrix.java
+++ src/com/datastructures/crackingthecodinginterview/arraysandstrings/ZeroMatrix.java
@@ -2,4 +2,45 @@ package com.datastructures.crackingthecodinginterview.arraysandstrings;
 
 public class ZeroMatrix {
 
+    private static void zeroMatrix(int[][] matrix) {
+        boolean firstRow = false, firstCol = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) firstRow = true;
+                    if (j == 0) firstCol = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRow) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        zeroMatrix(matrix);
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
 }
